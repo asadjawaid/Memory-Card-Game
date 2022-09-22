fa913151fb642c629e3167936fd30f25d9b70cd1
asad <asadjawaid99@hotmail.com>
2022-09-22 18:13:05 -0400

    Footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,15 @@
+import styles from '../styles/Footer.module.css';
+
 const Footer = () => {
-	return <p>Footer</p>;
+	return (
+		<footer className={styles['footer-container']}>
+			<p>
+				<a href="https://github.com/asadjawaid" target="_blank" rel="noopener noreferrer">
+					Asad Jawaid
+				</a>
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
