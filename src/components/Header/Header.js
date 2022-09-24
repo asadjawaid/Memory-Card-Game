2b74916af4156e0af0a0b0d4f542acf1deb9481f
asad <asadjawaid99@hotmail.com>
2022-09-24 17:23:21 -0400

    Prevent image draggin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
 		<header>
 			<div className={styles['header-container']}>
-				<img className={styles['header-container__image']} alt="Star Wars Logo" src={Logo} />
+				<img className={styles['header-container__image']} draggable={false} alt="Star Wars Logo" src={Logo} />
 				<h1 className={styles['header-container__title']}>
 					Memory <span>Game</span>
 				</h1>
