58283d8dcf85f5ac9609299111b71055f3e2d636
asad <asadjawaid99@hotmail.com>
2022-09-26 18:02:17 -0400

    Game over component and Character.css styled

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -1,9 +1,13 @@
+import styles from '../../styles/GameOver.module.css';
+
 const GameOver = ({ score, onStartNewGame }) => {
 	return (
-		<div>
-			<h1>Game Over!</h1>
+		<div className={styles['game-over-container']}>
+			<h1 className={styles['game-over-container__title']}>Game Over!</h1>
 			<p>Your score was {score}</p>
-			<button onClick={onStartNewGame}>Start a new game</button>
+			<button className={styles['game-over-container__btn']} onClick={onStartNewGame}>
+				Start a new game
+			</button>
 		</div>
 	);
 };
